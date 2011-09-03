0b0399540fc94091308e31f8ba3bfd31f55aadc0
Jeff Breidenbach <jbreiden@google.com>
2011-09-02 23:36:33 -0700

    checkpointing

--- monitor.py
+++ monitor.py
@@ -20,26 +20,62 @@ import time
 import pygame
 import sys
 import os.path
+import subprocess
+
+window = None
+
+def blue():
+  return (128, 128, 255)
+
+def render_text(screen, text, position):
+  font = pygame.font.SysFont('Courier', 28, bold=True)
+  text = font.render(text, 1, (255, 255, 255))
+  background = pygame.Surface(text.get_size())
+  background = background.convert()
+  background.fill(blue())
+  screen.blit(background, position)
+  screen.blit(text, position)
+
+def clearscreen(screen):
+  background = pygame.Surface(screen.get_size())
+  background = background.convert()
+  background.fill(blue())
+  screen.blit(background, (0,0))
 
 def main(barcode):
-  w = 1280
-  h = 800
+  global window
+  pygame.init()
+  beep = pygame.mixer.Sound('beep.wav')
+  h = pygame.display.Info().current_h
+  w = pygame.display.Info().current_w
+  fullscreen = True
   window = pygame.display.set_mode((w, h), pygame.FULLSCREEN)
-  screen = pygame.display.get_surface()
   current = ""
   while True:
     time.sleep(0.5)
     for event in pygame.event.get():
-      if (event.type == pygame.KEYUP) or (event.type == pygame.KEYDOWN):
-        window = pygame.display.set_mode((w, h))
+      if (event.type == pygame.KEYDOWN):
+        if event.key == pygame.K_ESCAPE or event.key == pygame.K_q: 
+          pygame.quit()
+          sys.exit()
+        if fullscreen:
+          window = pygame.display.set_mode((w, h))
+          fullscreen = False
+        else:
+          window = pygame.display.set_mode((w, h), pygame.FULLSCREEN)
+          fullscreen = True
+        current = ""
+      if event.type == pygame.QUIT:
+        pygame.quit()
         sys.exit()
     files = sorted(glob.glob('/var/tmp/playground/%s/*.pnm' % barcode))
     if len(files) < 2:
       continue
     latest = files[-1]
     if latest != current:
-      basename, extension = os.path.splitext(os.path.basename(latest))
-      if int(basename) % 2 == 1:
+      basename_a, dummy = os.path.splitext(os.path.basename(files[-2]))
+      basename_b, dummy = os.path.splitext(os.path.basename(files[-1]))
+      if int(basename_b) % 2 == 1:
         continue
       pygame.display.set_caption("Barcode: %s [ %s %s ]" %
                                  (barcode,
@@ -51,9 +87,16 @@ def main(barcode):
       surface_a = pygame.transform.smoothscale(image_a, (w_prime, h))
       surface_b = pygame.transform.smoothscale(image_b, (w_prime, h))
       surface_a = pygame.transform.flip(surface_a, True, False)
-      screen.blit(surface_a, (w / 2 - w_prime, 0))
-      screen.blit(surface_b, (w / 2, 0))
+      screen = pygame.display.get_surface()
+      clearscreen(screen)
+      epsilon = w // 100
+      screen.blit(surface_a, (w / 2 - w_prime - epsilon, 0))
+      screen.blit(surface_b, (w / 2 + epsilon, 0))
+      render_text(screen, basename_a, (0, 0))
+      render_text(screen, basename_b, (w - 110, 0))
       pygame.display.update()
+      beep.play()
       current = latest
 
-main(sys.argv[1])
+if __name__ == "__main__":
+  main(sys.argv[1])
